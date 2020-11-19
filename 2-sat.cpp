96caeed0cffb8c48648555bf2d28d79cc8f3d31d
soroush sahraei <55985277+sahroush@users.noreply.github.com>
2020-11-19 23:40:58 +0330

    Update 2-sat.cpp

--- 2-sat.cpp
+++ 2-sat.cpp
@@ -1,11 +1,11 @@
 struct twosat{ //~x = 2*x+1 , x = 2*x --> ~x = x^1
 	int n;
 	vector < vector < int > > in , out;
-	vector < int >  mark , col , topo , ans;
+	vector < int >  mark , col , topo;
 	int c = 0;
 	twosat(int N):
 		n(N) , in(n*2+5) , out(n*2+5) , mark(n*2+5 , 0) , col(n*2+5, 0) , ans(n + 5){}
-	bool operator [] (int x){ return(ans[x]);};
+	bool operator [] (int x){ return(col[2*i] > col[2*i + 1]);};
 	void add_edge(int v , int u){
 		in[u].pb(v) , out[v].pb(u);
 	}
@@ -21,7 +21,7 @@ struct twosat{ //~x = 2*x+1 , x = 2*x --> ~x = x^1
 			dfs(u);
 		topo.pb(v);
 	}
-	bool validate(){
+	inline bool validate(){
 		for(int i = 1 ; i <= 2*n + 1 ; i ++)if(!mark[i])dfs(i);
 		reverse(topo.begin() , topo.end());
 		memset(&mark[0] , 0 , sizeof(mark[0]) * mark.size());
@@ -29,7 +29,6 @@ struct twosat{ //~x = 2*x+1 , x = 2*x --> ~x = x^1
 			if(!mark[v])
 				c++ , sfd(v);
 		for(int i = 1 ; i <= n ; i ++)if(col[2*i] == col[2*i+1])return(0);
-		for(int i = 1 ; i <= n ; i ++)ans[i] = (col[2*i] > col[2*i + 1]);
 		return(1);
 	}
 };
