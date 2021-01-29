9a67858940972bbba1fb66b7ef1c9d866eadae3b
soroush sahraei <soroush.sahraei@gmail.com>
2021-01-29 15:31:59 +0330

    Update lca.cpp

--- lca.cpp
+++ lca.cpp
@@ -1,46 +1,34 @@
-int par[maxn][30];
-vector < int > adj[maxn];
-int h[maxn];
+const int lg = 20;
 
-void dfs(int v){
-    for (auto u : adj[v]){
-        if(u!=par[v][0]){
-            par[u][0] = v;
-            h[u] = h[v]+1;
-            dfs(u);
-        }
+struct LCA{
+	int n;
+	int par[lg+3][maxn] , h[maxn];
+	void dfs(int v  , int p , vector < int > *adj){
+		for(int u : adj[v]) if(u ^ p)
+			h[u] = h[v] + 1 , par[0][u] = v , dfs(u , v , adj);
 	}
-}
-
-int lca(int u , int v){
-    if(h[u] > h[v]){
-        swap(u , v);
-    }
-    for (int i = 35 ; i >= 0 ; i --){
-        if(par[v][i] and h[par[v][i]]>=h[u]){
-            v = par[v][i];
-        }
-    }
-    if(v == u){
-        return(v);
-    }
-    for (int i = 35 ; i >= 0 ; i --){
-        if(par[v][i] != par[u][i]){
-            v = par[v][i] , u = par[u][i];
-        }
-    }
-    return(par[v][0]);
-}
-
-void init(){
-	dfs(1);
-    for (int j = 1; j < 27 ; j ++){
-        for (int i = 1 ; i <= n ; i ++){
-            par[i][j] = par[par[i][j - 1]][j - 1];
-        }
-    }
-}
-
-int dist(int u , int v){
-	return(h[u] + h[v] - 2*h[lca(u, v)]);
-}
+	void init(int N , vector < int > *adj , int root = 1){
+		n = N;
+		dfs(root , 0 , adj);
+		h[0] = -1;
+		for(int j = 1 ; j <= lg ; j ++)
+			for(int i = 1 ; i <= n ; i ++)
+				par[j][i] = par[j - 1][par[j - 1][i]];
+	}
+	int lca(int u , int v){
+		if(h[u] < h[v])
+			swap(u , v);
+		for(int i = lg ; i >= 0 ; i --)
+			if(h[par[i][u]] >= h[v])
+				u = par[i][u];
+		if(u == v)
+			return(u);
+		for(int i = lg ; i >= 0 ; i --)
+			if(par[i][u] ^ par[i][v])
+				u = par[i][u] , v = par[i][v];
+		return(par[0][v]);
+	}
+	int dist(int u , int v){
+		return(h[u] + h[v] - 2*h[lca(u , v)]);
+	}
+};
